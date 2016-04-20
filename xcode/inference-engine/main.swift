2da32ca818a2260eaf0998e2b2612d722ba5021c
Alex Cummaudo <alexcu@me.com>
2016-04-20 17:13:03 +1000

    NEW: Initial commit with xcode template

---
+++ xcode/inference-engine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  inference-engine
+//
+//  Created by Alex on 20/04/2016.
+//  Copyright © 2016 Alex. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
