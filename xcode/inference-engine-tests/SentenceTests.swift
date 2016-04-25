99ddcfa3625f0c6d9c3c21b1c67983a93f5317be
Alex Cummaudo <alexcu@me.com>
2016-04-25 10:42:41 +1000

    NEW: Add Sentences

---
+++ xcode/inference-engine-tests/SentenceTests.swift
@@ -0,0 +1,72 @@
+//
+//  inference_engine_tests.swift
+//  inference-engine-tests
+//
+//  Created by Alex on 20/04/2016.
+//  Copyright © 2016 Alex. All rights reserved.
+//
+
+import XCTest
+
+class SentenceTests: XCTestCase {
+
+    func testNegate() {
+        // P    !P
+        // t     f
+        let p = Term(symbol: "P")
+        XCTAssertFalse(!p.truth)
+    }
+    func testConjoin() {
+        // P    Q   P & Q
+        // t    t     t
+        // t    f     f
+        // f    t     f
+        // f    f     f
+        let p = Term(symbol: "P")
+        let q = Term(symbol: "Q")
+        XCTAssertTrue((p & q).truth)
+        XCTAssertFalse((!p & !q).truth)
+        XCTAssertFalse((p & !q).truth)
+        XCTAssertFalse((!p & q).truth)
+    }
+    func testDisjoin() {
+        // P    Q   P | Q
+        // t    t     t
+        // t    f     t
+        // f    t     t
+        // f    f     f
+        let p = Term(symbol: "P")
+        let q = Term(symbol: "Q")
+        XCTAssertTrue((p | q).truth)
+        XCTAssertTrue((p | !q).truth)
+        XCTAssertTrue((!p | q).truth)
+        XCTAssertFalse((!p | !q).truth)
+    }
+    func testImplicates() {
+        // P    Q   P => Q
+        // t    t     t
+        // t    f     f
+        // f    t     t
+        // f    f     t
+        let p = Term(symbol: "P")
+        let q = Term(symbol: "Q")
+        XCTAssertTrue((p => q).truth)
+        XCTAssertTrue((!p => q).truth)
+        XCTAssertTrue((!p => !q).truth)
+        XCTAssertFalse((p => !q).truth)
+    }
+    func testBidirectionalImplicate() {
+        // P    Q   P <=> Q
+        // t    t      t
+        // t    f      f
+        // f    t      f
+        // f    f      t
+        let p = Term(symbol: "P")
+        let q = Term(symbol: "Q")
+        XCTAssertTrue((p <=> q).truth)
+        XCTAssertTrue((!p <=> !q).truth)
+        XCTAssertFalse((!p <=> q).truth)
+        XCTAssertFalse((p <=> !q).truth)
+    }
+
+}
---
+++ xcode/inference-engine/Sentence.swift
@@ -0,0 +1,73 @@
+//
+//  Sentence.swift
+//  inference-engine
+//
+//  Created by Alex on 20/04/2016.
+//  Copyright © 2016 Alex. All rights reserved.
+//
+
+// Represents logical entailment (does entail and does NOT entail)
+infix operator |=   { }
+infix operator |/=  { }
+
+// Represents logical biconditional
+infix operator <=> { }
+func <=>(lhs: Sentence, rhs: Sentence) -> Sentence {
+    return lhs.bidirectionallyImplicateWith(rhs)
+}
+
+// Represents logical implication
+infix operator  => { }
+func =>(lhs: Sentence, rhs: Sentence) -> Sentence {
+    return lhs.implicateWith(rhs)
+}
+
+// Represents logical disjunction (OR)
+func |(lhs: Sentence, rhs: Sentence) -> Sentence {
+    return lhs.disjoinWith(rhs)
+}
+
+// Represents logical conjunction (AND)
+func &(lhs: Sentence, rhs: Sentence) -> Sentence {
+    return lhs.conjunctWith(rhs)
+}
+// Represents logical disjunction (OR)
+prefix func !(lhs: Sentence) -> Sentence {
+    return lhs.negate()
+}
+
+struct Sentence {
+    let symbol: String
+    let truth: Bool
+
+    init(symbol: String, truth: Bool = true) {
+        self.symbol = symbol
+        self.truth = truth
+    }
+
+    func bidirectionallyImplicateWith(other: Sentence) -> Sentence {
+        // Bidirectional eliminiation equivalence
+        return (self => other) & (other => self)
+    }
+
+    func implicateWith(other: Sentence) -> Sentence {
+        // Implication elimination equivalence
+        return !self | other
+    }
+
+    func disjoinWith(other: Sentence) -> Sentence {
+        let symbol = "\(self.symbol)|\(other.symbol)"
+        let truth = self.truth || other.truth
+        return Sentence(symbol: symbol, truth: truth)
+    }
+
+    func conjunctWith(other: Sentence) -> Sentence {
+        let symbol = "\(self.symbol)&\(other.symbol)"
+        let truth = self.truth && other.truth
+        return Sentence(symbol: symbol, truth: truth)
+    }
+
+    func negate() -> Sentence {
+        return Sentence(symbol: "!\(self.symbol)", truth: !self.truth)
+    }
+}
